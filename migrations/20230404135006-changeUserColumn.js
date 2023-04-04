e432681daa7a4c727eecdf248df9a64bb5b5570d
joanbaldo <joanbaldo@hotmail.com>
2023-04-04 16:12:55 +0200

    Confirmed field added to Users table and config.as BOOLEAN

--- migrations/20230404135006-changeUserColumn.js
+++ migrations/20230404135006-changeUserColumn.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.addColumn("Users", "email", {
+    return queryInterface.addColumn("Users", "email", { // Mail field ADDED
       type:Sequelize.STRING,
       unique:true
       });
