d4a1f9a87a10a1cae9f1bb511497b12fdb84f855
joanbaldo <joanbaldo@hotmail.com>
2023-04-04 23:23:03 +0200

    CATEGORIES Endpoint Create Redo1

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,13 +1,41 @@
 const { Category } = require('../models/index.js');
 
+
 const CategoryController = {
 
-   
     create(req,res) {
-        Category.create(req.body)
-            // .then(category => res.status(201).send({ message: 'Categoria creada con éxito', category }))
-            .then(category => res.status(201).send(`La categoria -- ${category} -- ha sido creada con éxito`))
-            .catch((error) => console.error(error));
+         Category.create(req.body)
+            .then(post => res.status(201).send({ message: 'Categoria creada con éxito', post }))
+            .catch(console.error)
     },
-}    
+}
+
+
 module.exports = CategoryController
+
+
+
+
+
+
+
+
+
+
+
+
+// const { Category } = require('../models/index.js');
+
+// const CategoryController = {
+
+//     // ENDPOINT: CREAR CATEGORÍA
+//     create(req, res) {
+//         Category.create(req.body)
+//             // .then(category => res.status(201).send({ message: 'Categoria creada con éxito', category }))
+//             .then(category => res.status(201).send(`La categoria -- ${category} -- ha sido creada con éxito`))
+//             .catch((error) => console.error(error));
+//     },
+
+// }    
+
+// module.exports = CategoryController
--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,9 @@
-const express = require("express"); // Aquí nos importamos expres (siempre 1er paso)
+const express = require('express'); // Aquí nos importamos expres (siempre 1er paso)
 const router = express.Router(); // Importamos e inicializamos router
 
-const CategoryController = require("../controllers/CategoryController");  // Esto lo crea/importa automáticamente la linea 6
+const CategoryController = require('../controllers/CategoryController');  // Esto lo crea/importa automáticamente la linea 6
 
-router.post('/', CategoryController.create) // Creamos ruta URL para CREAR categoría
+router.post('/categoryC', CategoryController.create) // Creamos ruta URL para CREAR categoría
 
 
 
