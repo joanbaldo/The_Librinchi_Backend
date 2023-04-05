7bf99f45a73fb27a50b321e973788d40ff4b597e
joanbaldo <joanbaldo@hotmail.com>
2023-04-05 21:17:50 +0200

    PRODUCTS Endpoint SHOW by ID

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const ProductController = require('../controllers/ProductController');// Esto lo
 
 router.post('/productC', ProductController.create) // Creamos ruta URL para CREAR producto
 router.put('/productU/:id', ProductController.updateProduct) // Creamos ruta URL para CREAR producto
-
+router.get('/showPbyId/:id', ProductController.getById) // Creamos ruta URL para MOSTRAR las categorias por ID
 
 
 
